52d4155c0d3fa916228745f09ec02f89f5b272c9
ovrclock <serge.benissanh@cercaofficina.it>
2018-02-28 19:37:04 +0100

    modifiche diagrammi + relazione

--- gestione-orari/src/Interfaces/RisorsaInterface.ts
+++
@@ -1,8 +0,0 @@
-import DocenteModel from "../Models/Docente";
-
-export default interface RisorsaInterface{
-    name: string;
-    prenotata: Boolean;
-
-    prenota(prof: DocenteModel):Boolean;
-}
